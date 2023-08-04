1fbc4b4b4ce32964ec29e3c7743ea505083fff9f
sanjay-ranganath <ranganathsanjay@gmail.com>
2023-08-04 13:10:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const [count, setcount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Counter Application</h1>
+          <div className="my-5">
+            <h2 className="my-5">{count}</h2>
+            <button className="btn btn-info mx-3" onClick={()=>{setcount(count + 1)}}>increase</button>
+            <button className="btn btn-info mx-3" onClick={()=>{setcount(0)}}>reset</button>
+          </div>
+        </div>
+        <div className="my-3">
+          Car : Ford
+        </div>
+        <div className="my-3">
+          Type : Muscle
+        </div>
+        <div className="my-3">
+          Year of Manufacture : 1999
+        </div>
+      </div>
     </div>
   );
 }
